1beb740a1ae6747ee6a598a42326c2ea13836e89
sandeep-gowda-2003 <sandeeppp0001@gmail.com>
2024-09-30 17:44:45 +0530

    updating sampleApp

--- sampleApp/index.js
+++ sampleApp/index.js
@@ -1,12 +1,12 @@
 import hello from "./src/controllers/sam1.js";
 // import conn from "./src/data/db";
 import express from "express";
-// import env from "dotenv";
-// env.config();
+import env from "dotenv";
+env.config();
 
 let app = express();
 // let db = conn();
-connectToDatabase.app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("<h1> HELLO BUDDY I HOPE YOU ARE FINE </h1>");
 });
 app.get("/next", async (req, res) => {
