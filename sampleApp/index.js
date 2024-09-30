fefb8c79b095716251b7fcf7dd8026f40fce481a
sandeep-gowda-2003 <sandeeppp0001@gmail.com>
2024-09-30 17:38:14 +0530

    updating sampleApp

--- sampleApp/index.js
+++ sampleApp/index.js
@@ -1,15 +1,17 @@
-import conn from "./src/data/db";
+import hello from "./src/controllers/sam1";
+// import conn from "./src/data/db";
 import express from "express";
 // import env from "dotenv";
 // env.config();
 
 let app = express();
-let db = conn();
+// let db = conn();
 connectToDatabase.app.get("/", (req, res) => {
   res.send("<h1> HELLO BUDDY I HOPE YOU ARE FINE </h1>");
 });
 app.get("/next", async (req, res) => {
-  res.json(await (await db).collection("usercredentials").find());
+  //   res.json(await (await db).collection("usercredentials").find());
+  res.send(hello());
 });
 
 app.listen(process.env.PORT);
---
+++ sampleApp/src/controllers/sam1.js
@@ -0,0 +1,5 @@
+function hello() {
+  return "<h4>I hope you are fine</h4>";
+}
+
+export default hello;
